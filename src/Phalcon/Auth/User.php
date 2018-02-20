4ee0923e89dd730f1dd4e2efea0c57cf279ec911
eris2 <erisrayanesh@gmail.com>
2018-02-20 17:20:52 +0330

    1396-12-01 17:20 User model modified

--- src/Phalcon/Auth/User.php
+++ src/Phalcon/Auth/User.php
@@ -2,11 +2,11 @@
 
 namespace Phalcon\Auth;
 
-use Phalcon\Mvc\Model;
+use Phalcon\Mvc\AbstractModel;
 use Phalcon\Mvc\Model\Traits\Authenticatable as ModelAuthenticatable;
 use Phalcon\Mvc\Model\Traits\Authorizable as ModelAuthorizable;
 
-class User extends Model implements Authenticatable, Authorizable
+class User extends AbstractModel implements Authenticatable, Authorizable
 {
 	use ModelAuthorizable, ModelAuthenticatable;
 }
