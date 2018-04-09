60a4921b8eb1e102cfa737920b07234041c3ca65
eris2 <erisrayanesh@gmail.com>
2018-04-09 10:00:04 +0430

    1397-01-20 10:00 UserRegistration modified

--- src/Phalcon/Mvc/Controller/Traits/UserRegistration.php
+++ src/Phalcon/Mvc/Controller/Traits/UserRegistration.php
@@ -31,33 +31,7 @@ trait UserRegistration
 
 	protected function createUser($values)
 	{
-		try {
-			$filter = getSanitizer();
-
-			$user = new Users([
-				'fname' => strtolower($filter->sanitize($values['fname'],  'striptags')),
-				'lname' => strtolower($filter->sanitize($values['lname'],  'striptags')),
-				'email' => strtolower($filter->sanitize($values['email'], 'email')),
-				'password' => security()->hash($values['password'] . $this->getPasswordSalt()),
-				'active' => strtoupper(getSecurityRandom()->base58(5)),
-				'country_id' => $filter->sanitize($values['country_id'], 'int'),
-				'mobile' => $filter->sanitize($values['mobile'], 'int'),
-			]);
-
-			if ($user->save() === false){
-				throw new \RuntimeException();
-			}
-
-			return $user;
-		} catch (\RuntimeException $exc) {
-			return false;
-		}
-
-	}
-
-	protected function getPasswordSalt()
-	{
-		return 'jFr!!A&+71w1Ms9~8';
+		return null;
 	}
 
 	protected function onRegistrationFailed($data)
