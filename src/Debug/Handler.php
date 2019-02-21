080996d57c1ec3a73c92b7cd4f93bd03e9eb5017
eris2 <erisrayanesh@gmail.com>
2019-02-21 20:59:26 +0330

    Bugs fixed

--- src/Debug/Handler.php
+++ src/Debug/Handler.php
@@ -57,47 +57,38 @@ Class Handler implements ExceptionHandler
 
 	protected function renderUnhandledException(\Exception $exception)
 	{
-		($this->isDebugMode()?
-			$this->renderExceptionWithWhoops($exception) :
-			$this->renderFriendlyError($exception))->send();
+		return $this->isDebugMode()?
+					$this->renderExceptionWithWhoops($exception) :
+					$this->renderFriendlyError($exception);
 	}
 
 	protected function renderExceptionWithWhoops(\Exception $e)
 	{
 		$whoops = new Whoops();
+
 		if (request()->isAjax()){
 			$handler = new \Whoops\Handler\JsonResponseHandler();
 		} else {
 			$handler = new \Whoops\Handler\PrettyPageHandler();
 			$handler->handleUnconditionally(true);
-			$handler->addDataTable('Application (Request)', array(
-				'URI'         => request()->getScheme().'://' . request()->getServer('HTTP_HOST') . request()->getServer('REQUEST_URI'),
-				'Request URI' => request()->getServer('REQUEST_URI'),
-				'Path Info'   => request()->getServer('PATH_INFO'),
-				'HTTP Method' => request()->getMethod(),
-				'Script Name' => request()->getServer('SCRIPT_NAME'),
-				//'Base Path'   => $request->getBasePath(),
-				//'Base URL'    => $request->getBaseUrl(),
-				'Scheme'      => request()->getScheme(),
-				'Port'        => request()->getServer('SERVER_PORT'),
-				'Host'        => request()->getServer('HTTP_HOST'),
-			));
 		}
+
 		$whoops->pushHandler($handler);
 		$whoops->writeToOutput(false);
 		$whoops->allowQuit(false);
-		return new Response($whoops->handleException($e), 500, []);
+		return new Response($whoops->handleException($e), 500);
 	}
 
 	protected function renderFriendlyError(\Exception $e)
 	{
-		$response = new Response();
-		$response->setStatusCode(500);
+		$response = new Response('', 500);
+
 		if (request()->isAjax()){
 			$response->setJsonContent(["code" => 500, "message" => $e->getMessage()]);
 		} else {
 			$response->setContent("Whoops! Something went wrong");
 		}
+
 		return $response;
 	}
 
